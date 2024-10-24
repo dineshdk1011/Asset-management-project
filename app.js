b635cd19256b259443023a5b7c77230c8285ee5b
Dinesh <dineshkit15@gmail.com>
2024-10-24 17:50:59 +0530

    converted to pug

--- app.js
+++ app.js
@@ -22,7 +22,8 @@ app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
+app.set("view engine", "pug");
 app.set("views", path.join(__dirname, "views"));
 
 const sequelize = new Sequelize(
